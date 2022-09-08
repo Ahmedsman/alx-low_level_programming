e50bd1cc3ec53b2ab4a71478de6d80e79493fc92
Ahmedsman <ahmedsman11@gmail.com>
2022-09-08 15:36:18 -0700

    Task 5 commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main- The beginning of the program
+ *
+ *Return: without zero if no erros otherwise return non-zero value.
+ */
+int main(void)
+{
+		printf("\"with proper grammar, but the outcome is a piece of art,");
+			return (0);
+}
