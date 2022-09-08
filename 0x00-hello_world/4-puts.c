2f68e54d000d3ed167032a857f6111d97338c3cd
Ahmedsman <ahmedsman11@gmail.com>
2022-09-08 15:00:54 -0700

    Task 4 commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *Main
+ *Return: zero if no errors otherwise non-zero value.
+ */
+int main (void)
+{
+	 puts ("Programming is like building a multilingual puzzle!\ n");
+	 Return (0);
+}
