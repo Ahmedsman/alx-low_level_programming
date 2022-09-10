09717d62acaba46ee9b6e0213bc4c4eec7aceaa4
Ahmedsman <ahmedsman11@gmail.com>
2022-09-10 07:14:56 -0700

    Task 0 commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+/*
+ *File: positve_negative_or_zero.c
+ *Auth: Ahmed Suleiman
+ */
+
+#include <studio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/* my betty style for function main, if, else if, else*/
+
+int main (void)
+
+{		
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	if (n > 0)
+		
+		printf("%d is positive\n", n) ;
+	else if (n < 0)
+		printf("%d is negative\n", n) ;
+	else
+		print("%d is zero\n", n) ;
+	
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+On the 0x01. C - Variables, if, else, while; I performed task on the use of if, else if, else and while variable
