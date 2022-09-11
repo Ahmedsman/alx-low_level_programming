eef6418b243a76fb68dd571ef5a9ec167215d5a9
Ahmedsman <ahmedsman11@gmail.com>
2022-09-11 04:47:59 -0700

    Task 1 commit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,34 @@
+/*
+*File: 1-last_digit.c
+*Task 1
+*Auth: Ahmed Suleiman
+*/
+
+#include stdio.h
+#include stdlib.h
+#include time.h
+
+/*print the last digit of the number stored in the variable n
+*if the last digit is greater than 5, equal to  0 and if it is less than 6
+*/
+
+int main (void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	if ((n % 10) > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+	}
+	else if ((n % 10) < 6 && (n % 10) != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	}
+	
+	return (0);
