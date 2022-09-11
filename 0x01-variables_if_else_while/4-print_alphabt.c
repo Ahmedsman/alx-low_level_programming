0c52fc99597ab2ddc5cf6cff7fdd19ef8fb0d394
Ahmedsman <ahmedsman11@gmail.com>
2022-09-11 14:38:18 -0700

    Task 4 commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *
+ * main - entry piont
+ *
+ * description: using main function
+ * this programe print alphabet in lower case
+ * return: 0
+ */
+int main (void)
+{
+char ch
+for (ch = 'a' ; ch <= 'z' : ch++) 
+{
+	putchar (ch);
+}
+{
+	if (ch != 'e' && ch != 'q')
+{
+	putchar(ch);
+}
+}
+pitchar('\n');
+return(0);
+}
