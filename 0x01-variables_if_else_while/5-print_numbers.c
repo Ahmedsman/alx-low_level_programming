9cd4f53b6dd796c6a85e8394129d36ed31e5033e
Ahmedsman <ahmedsman11@gmail.com>
2022-09-12 03:10:22 -0700

    Task 5 commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit number of base 10 starting from 0
+ * All code will be in main function
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int dignum;
+
+	for (dignum = 0; dignum < 10; dignum++)
+			printf("%i", dignum);
+
+	printf("\n");
+
+	return (0);
+}
