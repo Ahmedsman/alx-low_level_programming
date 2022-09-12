50af6958a7ff1ab4800c6ed4d93516b7acd28788
Ahmedsman <ahmedsman11@gmail.com>
2022-09-12 07:37:45 -0700

    Task 10 commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,11 +11,11 @@ int main(void)
 {
 	int a, b, c;
 
-	for (a = '0'; <= '9'; a++)
+	for (a = '0'; a <= '9'; a++)
 	{
-		for (b = '0'; <= '9'; b++)
+		for (b = '0'; b <= '9'; b++)
 		{
-			for (c = '0'; <= '9'; c++)
+			for (c = '0'; c <= '9'; c++)
 			{
 				if (a < b && b < c)
 				{
