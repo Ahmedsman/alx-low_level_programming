71915ee2e86b5804906a472f8d0a1e000afb3d7c
Ahmedsman <ahmedsman11@gmail.com>
2022-09-12 04:02:41 -0700

    Task 6 commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio>
+
+/**
+ * main - print all single digit number of base 10 starting from 0
+ * instruction: use of char variable not allowe
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar(n + "0");
+
+	putchar("\n");
+
+	return (0);
+}
