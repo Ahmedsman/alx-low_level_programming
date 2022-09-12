2fcbc9af982648e87c5054c5620294f11545e0b7
Ahmedsman <ahmedsman11@gmail.com>
2022-09-12 05:44:09 -0700

    Task 9 commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combination of single number
+ * instruction: only putchar can be used for max 4
+ *
+ * return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+
+		if (i ! = '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
