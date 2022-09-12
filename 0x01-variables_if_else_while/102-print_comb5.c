2d61c2c9d45437d19f978daccdf7cc738b54b4ed
Ahmedsman <ahmedsman11@gmail.com>
2022-09-12 09:40:01 -0700

    Task 12 commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - print all possible combinations of two digit numbers.
+ * instruction: the number should range from 0 - 99
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int l, m, n, o;
+
+	for (l = 48; l <= 57; l++)
+	{
+		for (m = 48; m <= 57; m++)
+		{
+			for (n = 48; n <= 57; n++)
+			{
+				for (o = 48; o <= 57; o++)
+				{
+				if (((m + n) > (l + m) && m >= l) || l < m)
+				{
+					putchar(l);
+					putchar(m);
+					putchar(' ');
+					putchar(m);
+					putchar(n);
+					
+					if (l + m + n + o == 227 && l == 57)
+					{
+					break;
+					}
+					else
+					{
+					putchar(',');
+					putchar(' ');
+					}
+				}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
