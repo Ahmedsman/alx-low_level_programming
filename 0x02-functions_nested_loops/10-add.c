7fc716a3b3b25e11afb8ff178aeb76383db4c21d
Ahmedsman <ahmedsman11@gmail.com>
2022-09-14 03:59:16 -0700

    Task 10 commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - add integers
+ * @n1: integer 1
+ * @n2: integer 2
+ * description: add two integers and return the result
+ *
+ * Return: sum of integers
+ */
+
+int add(int n1, int n2)
+{
+	int sum;
+	
+	sum = n1 +n2;
+
+return: (sum);
+}
