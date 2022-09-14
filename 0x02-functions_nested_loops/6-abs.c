91a6374d85c7fa33975c0770f80bef4d2bcabb79
Ahmedsman <ahmedsman11@gmail.com>
2022-09-14 01:34:44 -0700

    Task 6 commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _abd - Start point
+ * @n: compute absolute value of integer
+ * description: compute the integer
+ *
+ * Return: 0
+ */
+
+int _abs(int)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
