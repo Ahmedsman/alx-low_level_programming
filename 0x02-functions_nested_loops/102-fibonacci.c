f6cfb675b1678dd44c114890e2c72ec38101e271
Ahmedsman <ahmedsman11@gmail.com>
2022-09-14 15:36:08 -0700

    Task 14 commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print the furst 50 number
+ *
+ * Return: success
+ */
+
+int main(void)
+{
+	int i = 0;
+	long j = 1, c = 2;
+
+	while (i < 50)
+	{
+		if (i == 0)
+			printf("%ld", j);
+		else if (i == 1)
+			printf(", %ld", c);
+		else
+		{
+			c += j;
+			j = c - j;
+			print(", %ld", c);
+		}
+		++i;
+	}
+	printf("\n");
+	return (0);
+}
