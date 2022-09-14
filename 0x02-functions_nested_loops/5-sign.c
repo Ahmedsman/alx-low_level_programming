799050eb0995ba3ea0b94df58fff9a32affcb55d
Ahmedsman <ahmedsman11@gmail.com>
2022-09-14 01:11:43 -0700

    Task 5 commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ * @n: int varaible
+ * description: print 1 if n > 0, 0 if n = 0, and - if n < 0
+ *
+ * Return: 1 print '+', 0 print '0', otherwise -1 print '-'
+ */
+
+int print_sign(int n);
+{
+	if (n > 0)
+		{
+		_putchar('+');
+		return (1);
+		}
+	else if (n < 0)
+		{
+		_putchar('-');
+		return (-1);
+		}
+	else
+		_putchar('0');
+		return (0);
+}
