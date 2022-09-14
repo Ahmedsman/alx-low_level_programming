78335106914c5cc7c9e4cd842c1cfc67f160e26d
Ahmedsman <ahmedsman11@gmail.com>
2022-09-13 20:58:56 -0700

    Task 1 commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - print alphabet in lower case
+ *
+ * Return: void
+ */
+
+void print_alphabet(void);
+{
+	char letter = 'a';
+
+	while (lettter <= 'z')
+	{
+		_putchr(letter);
+		letter++
+	}
+	_putchar('\n');
+}
