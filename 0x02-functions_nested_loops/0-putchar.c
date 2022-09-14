78e9750bc824a1716ac464c1656bb67e457799ec
Ahmedsman <ahmedsman11@gmail.com>
2022-09-13 20:30:44 -0700

    Task 0 commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,11 +8,10 @@
 
 int main(void)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114}
+	int str[] = {95, 112, 117, 116, 99, 104, 99, 114}
 	int count, sz;
 
 	sz = size of(str) / size of(int);
-
 	for(count = 0; count < sz; count++)
 	{
 		_putchar(str[count]);
