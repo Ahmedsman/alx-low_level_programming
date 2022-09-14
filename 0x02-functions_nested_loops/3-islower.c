e6a78c64e00e2777636c09e522b478eb1a5c5cfd
Ahmedsman <ahmedsman11@gmail.com>
2022-09-13 23:40:52 -0700

    Task 3 commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,26 @@
+#include "main"
+
+/**
+ * -islower.c - Entry poit
+ *  Description : print function that checks for lowercase character.
+ *
+ *  Return: 1 if true. 0 if false.
+ */
+
+int _islower(int c)
+{
+	int x = 'a';
+
+	for (x = 'a'; x<= 'z'; x++)
+	{
+		/* refer int*/
+		if (c == 1)
+		{
+			return (1);
+		}
+		{
+			return (0);
+		}
+	}
+}
+
