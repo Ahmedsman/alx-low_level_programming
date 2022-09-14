3d29e130d02523a0e79875838e7a6ed1b25518e2
Ahmedsman <ahmedsman11@gmail.com>
2022-09-13 23:20:03 -0700

    Task 2 commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10: print 10 times the alphabet
+ * in lowercase.
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char x;
+	int y = 0;
+
+	while (y <= 9)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+
+		y++;
+	}
+}
