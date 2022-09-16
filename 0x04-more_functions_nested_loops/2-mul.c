983db9c0d8cfe4e91ac88cd2750ab7c5485851b0
Ahmedsman <ahmedsman11@gmail.com>
2022-09-16 08:11:07 -0700

    Task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
  * mul - function that multiply 2 integers
  * @a: first integer
  * @b: second integer
  * Return: pucduct value of the integers
  */
-
 int mul(int a, int b)
 {
 	int mul(int a, int b)
