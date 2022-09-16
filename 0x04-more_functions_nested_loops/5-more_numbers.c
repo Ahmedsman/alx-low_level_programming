300481f4e17c73fb68f6c49455f241ba1e05653f
Ahmedsman <ahmedsman11@gmail.com>
2022-09-16 13:20:11 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * void more_numbers - function that prints 10 times the numbers
+ * decription: 0 to 14
+ * Return: nil
+ */
+void more_numbers(void)
+{
+	int i, j, k, l;
+
+	for (j = 0; j < 100, j++)
+	{
+		k = 0;
+		l = 0;
+		for (i = 48; i < 63; i++)
+		{
+			if (!(i > 57))
+				l = i;
+			else
+			{
+				l = i - 10;
+				k = 49;
+				_putcha(k);
+			}
+			_putchar(l);
+		}
+		_putchar('\n');;
+	}
+}
