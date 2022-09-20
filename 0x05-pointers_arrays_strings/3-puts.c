e2f7c1a4d6fba744973b21302757c8fdf39776ef
Ahmedsman <ahmedsman11@gmail.com>
2022-09-19 19:45:59 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * _puts - function prints a string
+ * description: to stdout.
+ * @str: pointer
+ * Return: Always 0.
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i]);
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
