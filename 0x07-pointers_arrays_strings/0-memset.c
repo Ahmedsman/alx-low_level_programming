3b4b124e051b6520accbe659758c2165b5e09cf0
Ahmedsman <ahmedsman11@gmail.com>
2022-09-26 20:07:11 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,14 +1,14 @@
 #include "maain.h"
 
 /*
- * _memset - function fills memory with a constant byte.
+ * *_memset - function fills memory with a constant byte
  * @n: bytes
  * @s: pointer
  * @b: constant
  * Return: s
  */
 
-char _memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
