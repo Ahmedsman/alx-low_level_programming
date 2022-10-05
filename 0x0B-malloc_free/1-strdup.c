61ca0fd47e8768b708959b85535404427fcec1a8
Ahmedsman <ahmedsman11@gmail.com>
2022-10-05 08:45:12 -0700

    Task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,10 +3,10 @@
 
 /**
  * _strdup - function returns a pointer to a newly
- * 	     allocated space in memory, which contains
- * 	     a copy of the string given as a parameter.
- * @str:     string
- * 
+ * allocated space in memory, which contains
+ * a copy of the string given as a parameter.
+ * @str: string
+ *
  * Return: NULL, otherwise a pointer to string.
  */
 
