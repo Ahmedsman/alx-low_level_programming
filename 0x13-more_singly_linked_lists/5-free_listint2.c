008d5c7cc28e1dc48cde5db3f6bfe5bb83a75450
Ahmedsman <ahmedsman11@gmail.com>
2022-10-25 14:30:36 -0700

    Task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,13 +12,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 	if (head == NULL)
-		return;
-	
+		return;	
 	while (*head)
 	{
 		temp = *head;
 		*head = (*head)->next;
 		free(temp);
 	}
-		head = NULL;
+	head = NULL;
 }
